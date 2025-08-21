7b44abfa732f936e06ad5a91371a8ee42c5a9af1
michaelgowdy <85449065+michaelgowdy@users.noreply.github.com>
2025-08-21 13:19:43 -0500

    removed unused code; header styling

--- js/components.js
+++ js/components.js
@@ -4,16 +4,43 @@ class THeader extends HTMLElement {
 	connectedCallback() {
 		this.innerHTML = `
 			<header>
-				<span>michael_gowdy</span>
-        <img src="img/ham-open.svg" class="ham" />
+				<a
+					id="title"
+					href="#"
+					onmouseover="swapText('title', 'ydwog_leahcim')"
+					onmouseout="swapText('title', 'michael_gowdy')"
+					onclick="toggleContactToWork()"
+				>
+					michael_gowdy
+				</a>
+
 				<nav id="navbar">
-					<a href="#">work</a>
-					<a href="#">contact</a>
-					<button onclick="toggleTheme()">
-						<span class="material-symbols-outlined orange600">
-							visibility
+					<a
+						id="work"
+						href="#work"
+						onmouseover="swapText('title', 'krow')"
+						onmouseout="swapText('title', 'michael_gowdy')"
+						onclick="toggleWorkToContact()"
+					>
+						work
+					</a>
+
+					<a
+						id="contact"
+						class="hidden"
+						href="#contact"
+						onmouseover="swapText('title', 'tcatnoc')"
+						onmouseout="swapText('title', 'michael_gowdy')"
+						onclick="toggleContactToWork()"
+					>
+						contact
+					</a>
+
+					<a href="#" class="theme-toggle" onclick="toggleTheme()">
+						<span class="material-symbols-outlined">
+								contrast
 						</span>
-					</button>
+					</a>
 				</nav>
 			</header>
 		`;
@@ -21,118 +48,31 @@ class THeader extends HTMLElement {
 }
 customElements.define("t-header", THeader);
 
-// section
-class TSection extends HTMLElement {
-  connectedCallback() {
-    const heading = this.getAttribute("heading");
-    const details = this.getAttribute("details");
-    const link = this.getAttribute("link");
-    const linkText = this.getAttribute("linkText");
-    const img = this.getAttribute("img");
-    const heading2 = this.getAttribute("heading2");
-    const details2 = this.getAttribute("details2");
-    const link2 = this.getAttribute("link2");
-    const linkText2 = this.getAttribute("linkText2");
-    const img2 = this.getAttribute("img2");
-
-    this.innerHTML = `
-      <section class="flex margin6">
-        <div>
-          <h2>${heading}</h2>
-          <p>${details}</p>
-          <a class="btn" href="${link}">${linkText}</a>
-        </div>
-        <img src="${img}"/>
-      </section>
-      <section class="flex margin6 reverse">
-        <div>
-          <h2>${heading2}</h2>
-          <p>${details2}</p>
-          <a class="btn" href="${link2}">${linkText2}</a>
-        </div>
-        <img src="${img2}"/>
-      </section>
-    `;
-  }
-}
-customElements.define("t-section", TSection);
-
 // main section
-class TMainSection extends HTMLElement {
-  connectedCallback() {
-    const heading = this.getAttribute("heading");
-    const details = this.getAttribute("details");
+// class TMainSection extends HTMLElement {
+//   connectedCallback() {
+//     const heading = this.getAttribute("heading");
+//     const details = this.getAttribute("details");
 
-    this.innerHTML = `
-      <div class="main-section">
-        <h2>${heading}</h2>
-        <p>${details}</p>
-      </div>
-    `;
-  }
-}
-customElements.define("t-main-section", TMainSection);
-
-// CTA
-class TCTA extends HTMLElement {
-  connectedCallback() {
-    const heading = this.getAttribute("heading");
-    const details = this.getAttribute("details");
-    const linkText = this.getAttribute("linkText");
-    const link = this.getAttribute("link");
-
-    this.innerHTML = `
-      <section class="center padding6">
-        <h2>${heading}</h2>
-        <p>${details}</p>
-        <a href="${link}" class="btn">${linkText}</a>
-      </section>
-    `;
-  }
-}
-customElements.define("t-cta", TCTA);
-
-// footer
-class TFooter extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = `
-      <footer>
-        <div class="footer">
-          <div class="footer-left">
-            <img class="logo" src="img/almost logo.png" />
-            <p>
-              We build websites that load in under a minute. <br />
-              Sometimes even faster.
-            </p>
-          </div>
-          <div class="footer-right">
-            <div class="social-media-icons">
-              <img src="img/icons8-github.svg" class="social-media" />
-              <img src="img/icons8-linkedin.svg" class="social-media" />
-              <img src="img/icons8-x.svg" class="social-media" />
-              <img src="img/icons8-youtube.svg" class="social-media" />
-            </div>
-            <p>
-              3 Way, 69th Street, Somewhere, USB <br />
-              123-456-7890 <br />
-              bye@almost.dev
-            </p>
-          </div>
-        </div>
-      </footer>
-    `;
-  }
-}
-customElements.define("t-footer", TFooter);
+//     this.innerHTML = `
+//       <div class="main-section">
+//         <h2>${heading}</h2>
+//         <p>${details}</p>
+//       </div>
+//     `;
+//   }
+// }
+// customElements.define("t-main-section", TMainSection);
 
 // hamburger
-const ham = document.querySelector(".ham");
-const nav = document.querySelector("nav");
-ham.addEventListener("click", toggle);
-nav.addEventListener("click", toggle);
-function toggle() {
-  ham.src = ham.src.includes("img/ham-close.svg")
-    ? "img/ham-open.svg"
-    : "img/ham-close.svg";
-  nav.classList.toggle("show");
-}
+// <img src="img/ham-open.svg" class="ham" />
+// const ham = document.querySelector(".ham");
+// const nav = document.querySelector("nav");
+// ham.addEventListener("click", toggle);
+// nav.addEventListener("click", toggle);
+// function toggle() {
+//   ham.src = ham.src.includes("img/ham-close.svg")
+//     ? "img/ham-open.svg"
+//     : "img/ham-close.svg";
+//   nav.classList.toggle("show");
+// }
--- js/utils.js
+++ js/utils.js
@@ -2,3 +2,27 @@ function toggleTheme() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+
+function showElement(id) {
+	var element = document.getElementById(id);
+	element.classList.remove('hidden');
+}
+
+function hideElement(id) {
+	var element = document.getElementById(id);
+	element.classList.add('hidden');
+}
+
+function toggleWorkToContact() {
+	showElement('contact');
+	hideElement('work');
+}
+
+function toggleContactToWork() {
+	showElement('work');
+	hideElement('contact');
+}
+
+function swapText(id, newText) {
+	document.getElementById(id).textContent = newText;
+}
